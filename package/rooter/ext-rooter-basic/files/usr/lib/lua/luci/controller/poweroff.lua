87362563e0450b9741dfb3ad88135e4865188e4e
xiaoxifu64 <49739269+xiaoxifu64@users.noreply.github.com>
2022-01-08 00:13:19 +0800

    update file poweroff.lua

--- package/rooter/ext-rooter-basic/files/usr/lib/lua/luci/controller/poweroff.lua
+++ package/rooter/ext-rooter-basic/files/usr/lib/lua/luci/controller/poweroff.lua
@@ -2,7 +2,7 @@ module("luci.controller.poweroff", package.seeall)
 
 function index()
         local page
---        page = entry({"admin", "system", "poweroff"}, template("admin_system/poweroff"), _("System Stop"), 95)
+        page = entry({"admin", "system", "poweroff"}, template("admin_system/poweroff"), _("System Stop"), 95)
         entry({"admin", "system", "do_poweroff"}, call("action_poweroff"))
         page.dependent = true
 end
